be4c2fd6e1aa0a62b248343927a562f03f253b28
ileegant <osukhoruchko@gmail.com>
2026-02-19 17:01:45 +0200

    Add difficulty selection and timer to the game

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import useGenerateCards from "./hooks/useGenerateCards";
 import Card from "./components/ui/Card";
+import { ListOfDifficulties } from "./data/config";
+import Timer from "./components/ui/Timer";
 
 function App() {
-  const [count, setCount] = useState(64);
+  const [count, setCount] = useState(64); // diff easy: 4 medium: 16 hard: 36 extreem: 64
   const { cards, onFlip } = useGenerateCards({ count });
-  const cols = Math.sqrt(count);
+  const cols = Math.ceil(Math.sqrt(count));
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
+      <select
+        name="difficulty"
+        id="diff"
+        value={count}
+        onChange={(e) => setCount(Number(e.target.value))}
+      >
+        <option value={ListOfDifficulties.easy}>Easy (2x2)</option>
+        <option value={ListOfDifficulties.medium}>Medium (4x4)</option>
+        <option value={ListOfDifficulties.hard}>Hard (6x6)</option>
+        <option value={ListOfDifficulties.extreme}>EXTREME (8x8)</option>
+      </select>
+
+      <Timer />
+
       <div className="w-min p-10">
         <div
           style={{
